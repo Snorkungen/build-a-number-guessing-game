73a1fe95978fa6ea974785848d05194e8b4c0977
Snorkungen <teuvo2004@gmail.com>
2022-08-20 11:02:17 +0000

    feat: added welcomes user

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-NUMBER=$(($RANDOM % 50))
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-echo $NUMBER
+NUMBER=$(($RANDOM % 1000 + 1))
+
+echo "Enter your username:"
+read NAME
+
+USER=$($PSQL "SELECT name, COUNT(game_id), MIN(turns) FROM games WHERE name = '$NAME' GROUP BY name;")
+
+if [[ -z $USER ]];then 
+  echo "Welcome, $NAME! It looks like this is your first time here."
+else 
+  echo $USER | while IFS=\| read NAME COUNT BEST; do
+    echo "Welcome back, $NAME! You have played $COUNT games, and your best game took $BEST guesses."
+  done
+fi
